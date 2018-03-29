c64f7df40201e1716d5c91028a21d5d7211576e6
Alexy <alexandra.fina@yahoo.com>
2018-03-29 20:42:13 +0100

    Refactor error test

--- jasmine/src/gameNumber.js
+++ jasmine/src/gameNumber.js
@@ -1,11 +1,14 @@
 var GameNumber = function () {
   this.inputNumber = 0;
   this.history = [];
-  this.error = "You must click on all the options from 1-6"
+  this.error = "";
 };
 
 GameNumber.prototype.submitData = function (number) {
   this.inputNumber = number;
+  if (this.history != 6) {
+    return this.error = "You must click on all the options from 1-6";
+  };
 };
 
 GameNumber.prototype.clickOption = function (number) {
