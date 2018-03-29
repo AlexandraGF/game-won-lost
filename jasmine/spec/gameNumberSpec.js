eb13579d8c2bc2d3ebf0694aeca23d5f31b09662
Alexy <alexandra.fina@yahoo.com>
2018-03-29 20:19:04 +0100

    Test pass for input number from user

--- jasmine/spec/gameNumberSpec.js
+++ jasmine/spec/gameNumberSpec.js
@@ -1,8 +1,8 @@
-describe('gameNumber', function (){
+describe('GameNumber', function (){
 
   describe('The user can ', function(){
     it('input a number between 10 and 60', function() {
-      game= new gameNumber();
+      game = new GameNumber();
       game.submitData(12);
       expect(game.inputNumber).toEqual(12);
     });
---
+++ jasmine/src/gameNumber.js
@@ -0,0 +1,7 @@
+var GameNumber = function () {
+  this.inputNumber = 0;
+};
+
+GameNumber.prototype.submitData = function (number) {
+  this.inputNumber = number;
+};
