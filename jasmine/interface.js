34d270b739ea19d42f106ebede733ce325a2a0dd
Alexy <alexandra.fina@yahoo.com>
2018-03-29 21:41:26 +0100

    Add clicked when a user clicked a number and css

--- jasmine/interface.js
+++ jasmine/interface.js
@@ -3,6 +3,13 @@ $(document).ready(function() {
 
   $('#button1').one('click', function() {
     game.clickOption(1);
+    $('#clicked').text("Clicked");
     console.log(game.history);
   });
+
+  $('#button2').one('click', function() {
+    game.clickOption(2);
+    console.log(game.history);
+  });
+
 });
