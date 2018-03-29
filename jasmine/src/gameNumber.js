850de75bd1be6f622abeed42df1efae40aacfd91
Alexy <alexandra.fina@yahoo.com>
2018-03-29 20:29:20 +0100

    Test user can choose a number options passing

--- jasmine/src/gameNumber.js
+++ jasmine/src/gameNumber.js
@@ -1,7 +1,12 @@
 var GameNumber = function () {
   this.inputNumber = 0;
+  this.history = [];
 };
 
 GameNumber.prototype.submitData = function (number) {
   this.inputNumber = number;
+};
+
+GameNumber.prototype.clickOption = function (number) {
+  this.history.push(number);
 };
